b13ebd6bfccf2a44cf363bf938e2a0c1b0e5a828
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2025-10-13 19:24:52 +0200

    feat: start from modified Saral Theme
    https://github.com/yashjawale/saral-theme-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,42 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import partytown from '@astrojs/partytown'
+import icon from 'astro-icon'
+import rehypeFigureTitle from 'rehype-figure-title'
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-});
+	site: 'https://blog.pablopl.dev',
+
+	integrations: [
+		mdx(),
+		sitemap(),
+		icon(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+	],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+
+	markdown: {
+		remarkPlugins: [remarkReadingTime, remarkModifiedTime],
+		rehypePlugins: [rehypeFigureTitle, rehypeAccessibleEmojis],
+	},
+
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+})
