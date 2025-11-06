eeebe6accedb92f1dd06708c0df14fd4d3a1c823
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2025-11-06 17:55:40 +0100

    chore: Delete Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
 import icon from 'astro-icon'
 import rehypeFigureTitle from 'rehype-figure-title'
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
@@ -43,11 +42,6 @@ export default defineConfig({
 			},
 		}),
 		icon(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
 	],
 
 	vite: {
