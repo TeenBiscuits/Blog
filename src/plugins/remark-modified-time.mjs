a690b9aded426f686bbad80284a7c2e69da88660
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2025-11-13 22:00:08 +0100

    fix: set lastModified to default if fails

--- src/plugins/remark-modified-time.mjs
+++ src/plugins/remark-modified-time.mjs
@@ -4,6 +4,7 @@ export function remarkModifiedTime() {
 	return function (_, file) {
 		const filepath = file.history[0]
 		const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`)
-		file.data.astro.frontmatter.lastModified = result.toString()
+		file.data.astro.frontmatter.lastModified =
+			result.toString() || '2025-01-01T00:00:00Z'
 	}
 }
